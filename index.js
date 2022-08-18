fbf1b51bf4d5ef5eca79ce81f700d2cee2b454db
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-18 12:23:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+  const superbowlWin = (records) => {
+    const foundResult = records.find((record)=>{
+        return record.result === "W";
+    })
+
+    if(foundResult){
+        return foundResult.year;
+    }else{
+        return undefined;
+    }
+  }
